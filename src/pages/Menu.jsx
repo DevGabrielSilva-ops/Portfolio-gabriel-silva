108fa8ea4384e1135ca2d59c514a8bc91129e0ed
Gabriel Silva <dev13.gabriel@gmail.com>
2026-05-11 16:43:09 -0300

    Corrigindo erro do deploy

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,5 +1,5 @@
 import Header from '../components/header/header'
-import Infos from '../components/infos/infos'
+import Infos from '../components/Infos/info'
 import Sobre from '../components/Sobre/sobre'
 import Habilidades from '../components/habilidades/habilidades'
 import Projetos from '../components/projetos/projetos'
